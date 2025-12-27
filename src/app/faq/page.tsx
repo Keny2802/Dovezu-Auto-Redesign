66b7f5902fa7732cb6aecbb43cce017e80b91c1e
Keny2802 <oliva.vojta@gmail.com>
2025-12-27 02:08:18 +0100

    Přidání nových změn.

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,15 @@
+import {
+    Fragment
+} from "react";
+
+import Wrapper from "../Components/Wrapper";
+
+const Page = () => {
+    return (
+        <Fragment>
+            <Wrapper className="px-6 md:px-8 lg:px-10 py-8 md:py-10 lg:py-12"></Wrapper>
+        </Fragment>
+    );
+};
+
+export default Page;
